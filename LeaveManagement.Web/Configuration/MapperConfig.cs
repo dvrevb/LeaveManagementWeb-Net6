57e6486f42ae7559b858f57310335c0abe78d3f2
dvrevb <dvrevb@outlook.com>
2022-08-20 01:46:16 +0300

    AutoMapper added. Fontawesome added. Datatable added. Sweet alert added. Modified crud pages.

---
+++ LeaveManagement.Web/Configuration/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ForMember(dest => dest.LeaveTypeId, opt => opt.MapFrom(src => src.Id)).ReverseMap();    // source dest
+        }
+        
+    }
+}
